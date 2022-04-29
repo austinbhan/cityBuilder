e983ddf6b9398a0e3b3440ac38d46a65ee28d473
austinbhan <austinbhan@outlook.com>
2022-04-28 17:56:02 -0700

    First selection picture working

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// import functions and grab DOM elements
+// import functions and grab DOM elements //Update DOM Elements to Match
 const foodDropDown = document.getElementById('foodDropDown');
 const transportDropDown = document.getElementById('transportDropDown');
 const parkingDropDown = document.getElementById('parkingDropDown');
@@ -19,9 +19,30 @@ let foodChange = 0;
 let transportChange = 0;
 let parkingChange = 0;
 
+let textArray = [0];
+
 // set event listeners
 
+foodDropDown.addEventListener('change', () => {
+   // get user input
+  console.log('you selected', foodDropDown.value);
+  // use user input to update state 
+  foodChange++;
+  // update DOM to reflect the new state
+  foodPic.style.backgroundImage = `url('./assets/${foodDropDown.value}-food.png')`;
+});
 
+transportDropDown.addEventListener('change', () => {
+  // get user input
+  console.log('you selected', transportDropDown.value);
+  // use user input to update state 
+  // update DOM to reflect the new state
+
+});
+
+parkingDropDown.addEventListener('change', () => {
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+});
