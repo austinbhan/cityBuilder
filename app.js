71bbb1f0062a6a0b7e1d58fd9244a720edd55559
austinbhan <austinbhan@outlook.com>
2022-04-28 17:19:11 -0700

    Starting event listeners

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 // import functions and grab DOM elements
+const foodDropDown = document.getElementById('foodDropDown');
+const transportDropDown = document.getElementById('transportDropDown');
+const parkingDropDown = document.getElementById('parkingDropDown');
+
+const foodPic = document.getElementById('foodsie');
+const transportPic = document.getElementById('transportie');
+const parkPic = document.getElementById('parkingie');
+
+const textField = document.getElementById('textField');
+const textButton = document.getElementById('textButton');
+
+const selectionRecord = document.getElementById('selectionRecord');
+const textFieldRecord = document.getElementById('textFieldRecord');
 
 // let state
 
-// set event listeners 
+let foodChange = 0;
+let transportChange = 0;
+let parkingChange = 0;
+
+// set event listeners
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
